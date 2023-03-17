db9bb18541a3d50fe175ddd9080f2ceddc6cd86f
ZeroToH3ro <mtblaser2002@gmail.com>
2023-03-17 22:33:00 +0700

    first commit 2

---
+++ FirstDay/index.js
@@ -0,0 +1,29 @@
+//Knowledge need to learn
+//Frontend: DOM manipulation, event, callback, ajax call, json 
+//Nodejs: require, async, node package, npm 
+//ES6 => async, await, promise
+
+
+//Basic knowledge
+//Print Name
+console.log("Hello World")
+//Variable
+let name = "Minh"
+console.log("Hello World", name)
+//Object
+let minh = {
+    name: "Trinh Ngoc Minh",
+    age: 20,
+    men: true
+}
+console.log(minh)
+console.log(minh.age)
+//Array
+const fruit = new Array(4);
+fruit[0] = "mango"
+fruit[1] = "butter"
+fruit[2] = "dragon fruit"
+fruit[3] = "strawberry"
+console.log(fruit)
+
+
